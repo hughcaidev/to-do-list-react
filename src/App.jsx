cda69b5b10c954983a6ee9a585c5a1c643d8325a
Hugh <hughcaidev@gmail.com>
2022-12-13 20:00:26 +0000

    add complete task feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import reactLogo from "./assets/react.svg"
 // import './App.css'
+
+const initialToDoList = [
+    { task: "Task 1", key: 1, complete: false },
+    { task: "Task 2", key: 2, complete: true },
+    { task: "Task 3", key: 3, complete: false },
+]
 
 const generateKey = (pre) => {
     return `${pre}_${new Date().getTime()}`
 }
 
 function App() {
-    const [items, setItems] = useState([])
+    const [items, setItems] = useState(initialToDoList)
     const inputRef = useRef()
 
     function onSubmit(e) {
@@ -18,7 +24,7 @@ function App() {
         if (task === "") return
 
         const key = generateKey(task)
-        const item = { key, task }
+        const item = { key, task, complete: false }
 
         setItems((prev) => [...prev, item])
 
@@ -29,24 +35,68 @@ function App() {
         setItems(items.filter((i) => i != item))
     }
 
+    function toggleTaskCompleted(item) {
+        const newList = items.map((i) => {
+            if (i == item) {
+                return { ...i, complete: !i.complete }
+            }
+            return i
+        })
+
+        setItems(newList)
+    }
+
     return (
         <div className="App">
             <form onSubmit={onSubmit}>
                 New Item: <input type="text" ref={inputRef}></input>
                 <button type="submit">Add</button>
             </form>
+            <h2>In Progress</h2>
             <ul>
-                {items.map((item, index) => {
-                    const { key, task } = item
-                    return (
-                        <li key={key}>
-                            {task}
-                            <button onClick={() => removeItemFromList(item)}>
-                                &times;
-                            </button>
-                        </li>
-                    )
-                })}
+                {items
+                    .filter((item) => !item.complete)
+                    .map((item, index) => {
+                        const { key, task, complete } = item
+                        return (
+                            <li key={key}>
+                                {task}
+                                <button
+                                    onClick={() => toggleTaskCompleted(item)}
+                                >
+                                    Complete
+                                </button>
+                                <button
+                                    onClick={() => removeItemFromList(item)}
+                                >
+                                    &times;
+                                </button>
+                            </li>
+                        )
+                    })}
+            </ul>
+            <h2>Completed</h2>
+            <ul>
+                {items
+                    .filter((item) => item.complete)
+                    .map((item, index) => {
+                        const { key, task, complete } = item
+                        return (
+                            <li key={key}>
+                                {task}
+                                <button
+                                    onClick={() => toggleTaskCompleted(item)}
+                                >
+                                    In Progress
+                                </button>
+                                <button
+                                    onClick={() => removeItemFromList(item)}
+                                >
+                                    &times;
+                                </button>
+                            </li>
+                        )
+                    })}
             </ul>
         </div>
     )
