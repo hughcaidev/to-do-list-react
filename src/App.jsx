2397d005e4bf15453549e8bc4358bdc7c15b9835
Hugh <hughcaidev@gmail.com>
2022-12-13 19:01:01 +0000

    add feature to remove items from to-do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useRef, useState } from "react"
+import reactLogo from "./assets/react.svg"
 // import './App.css'
 
+const generateKey = (pre) => {
+    return `${pre}_${new Date().getTime()}`
+}
+
 function App() {
-  const [items, setItems] = useState([])
-  const inputRef = useRef()
+    const [items, setItems] = useState([])
+    const inputRef = useRef()
 
-  function onSubmit(e){
-    e.preventDefault()
+    function onSubmit(e) {
+        e.preventDefault()
 
-    const item = inputRef.current.value
+        const task = inputRef.current.value
 
-    if (item === "") return
-    setItems(prev => [...prev, item])
+        if (task === "") return
 
-    inputRef.current.value = ""
-  }
+        const key = generateKey(task)
+        const item = { key, task }
 
-  return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        New Item: <input type="text" ref={inputRef}></input>
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {items.map(item => {
-          return <li>{item}</li>
-        })}
-      </ul>
-    </div>
-  )
+        setItems((prev) => [...prev, item])
+
+        inputRef.current.value = ""
+    }
+
+    function removeItemFromList(item) {
+        setItems(items.filter((i) => i != item))
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={onSubmit}>
+                New Item: <input type="text" ref={inputRef}></input>
+                <button type="submit">Add</button>
+            </form>
+            <ul>
+                {items.map((item, index) => {
+                    const { key, task } = item
+                    return (
+                        <li key={key}>
+                            {task}
+                            <button onClick={() => removeItemFromList(item)}>
+                                &times;
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
 export default App
