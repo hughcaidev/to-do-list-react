4956db783809734625d471de572bda562a12f3d3
Hugh <hughcaidev@gmail.com>
2022-12-13 18:43:00 +0000

    create to do list boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([])
+  const inputRef = useRef()
+
+  function onSubmit(e){
+    e.preventDefault()
+
+    const item = inputRef.current.value
+
+    if (item === "") return
+    setItems(prev => [...prev, item])
+
+    inputRef.current.value = ""
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={onSubmit}>
+        New Item: <input type="text" ref={inputRef}></input>
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {items.map(item => {
+          return <li>{item}</li>
+        })}
+      </ul>
     </div>
   )
 }
