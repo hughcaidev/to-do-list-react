d69a69fb0587fa456dd79c5d6ed4084263332e50
Hugh Cai <hughcaidev@gmail.com>
2023-02-26 12:26:48 +0000

    feat: update add task form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TaskList from './components/ToDoList'
 import { useTaskList } from './context/TaskListContext'
 import AddTaskForm from './components/AddTaskForm'
@@ -12,13 +12,22 @@ function App() {
     const { items } = useTaskList()
     const { edittedTask, setEdittedTask } = useEdittedTask()
 
+    const [isTaskFormVisible, setTaskFormVisibility] = useState(false)
+
     const activeTasks = items.filter((item) => !item.complete)
     const completedTasks = items.filter((item) => item.complete)
 
     return (
         <div className="App">
             <div className="to-do-list">
-                <AddTaskForm />
+                {!isTaskFormVisible ? (
+                    <button onClick={() => setTaskFormVisibility(true)}>Add Task</button>
+                ) : (
+                    <div>
+                        <button onClick={() => setTaskFormVisibility(false)}>&times;</button>
+                        <AddTaskForm />
+                    </div>
+                )}
                 <TaskList
                     tasks={activeTasks}
                     title="In Progress"
